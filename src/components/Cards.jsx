bdd7b048db28eb8ff62e3cf6e6fbc89465ce41d0
Vince Hartman <vhartman11@gmail.com>
2023-03-19 20:26:36 -0400

    first commit to this branch. cleand up files by removing extra CSS files, mostly related to all the card pages.  Tried to work on video some too.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,15 +6,20 @@ function Cards() {
   return (
     <div className='cards'>
       <h1>Check out some of my favorite projects and things to do!</h1>
+
+
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
+            
             <CardItem
               src='images/certificateTN.png'
               alt='my MIT certificate in coding'
               text='My Professional Certificate in Coding that I received in October 2022.'
               label='My Certificate'
               path='/certificate'
+              target=''
+              rel=''
             />
             <CardItem
               src='images/hobbies/logo.jpg'
@@ -22,6 +27,8 @@ function Cards() {
               text='Creating a website for my homestead will become my next coding project.'
               label='Coding'
               path='/aboutMe'
+              target=''
+              rel=''
             />
           </ul>
           <ul className='cards__items'>
@@ -31,6 +38,8 @@ function Cards() {
               text='This 9.48 pound carp was caught on the French Broad River in Tennessee.'
               label='Fishing'
               path='/aboutMe'
+              target=''
+              rel=''
             />
             <CardItem
               src='images/projects/eyes.png'
@@ -38,6 +47,8 @@ function Cards() {
               text='One of my first projects. The eyes follow the movements of your mouse.'
               label='A fun project'
               path='/projects'
+              target=''
+              rel=''
             />
             <CardItem
               src='images/projects/map.png'
@@ -45,6 +56,8 @@ function Cards() {
               text='This map updates every 15 seconds to show buses on a particular route.'
               label='Real time mapping'
               path='/projects'
+              target=''
+              rel=''
             />
           </ul>
         </div>
--- src/components/pages/AboutMeCards.jsx
+++ src/components/pages/AboutMeCards.jsx
@@ -1,45 +1,54 @@
 import React           from 'react';
-import AboutMeCardItem from './AboutMeCardItem';
-import './AboutMeCards.css';
+import CardItem from '../CardItem';
+import '../Cards.css';
 
 function AboutMeCards() {
   return (
     <div className='cards'>
       <h1>Check out some of my favorite things to do!</h1>
+
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             
-            <AboutMeCardItem
+            <CardItem
               src='images/hobbies/logo.jpg'
               alt='this is the logo for the next website that i want to build'
               text='My next web development project would be to build a website for our homestead.  On our homestead we have chickens, honey bees, fruit and nut trees, and a garden.  I would want to incorperate these things into the site by using many forms of media.  At some point in the future we may sell goods from the site.'
               label='Website Building'
-              path='#'
+              path=''
+              target=''
+              rel=''
             />
-            <AboutMeCardItem
+            <CardItem
               src='images/hobbies/fish.jpg'
               alt='9.48 pound carp'
               text='I enjoy fishing for all species of fish but usually target bass, trout, and catfish.  In this photo, I am holding a 9.48 pound carp that was caught on the French Broad River in Tennessee on a family vacation.  This is the largest fish I have caught.'
               label='Fishing'
-              path='#'
+              path=''
+              target=''
+              rel=''
             />
           </ul>
 
           <ul className='cards__items'>
-            <AboutMeCardItem
+            <CardItem
               src='images/hobbies/carpentry.jpg'
               alt='a complete bee hive that I built'
               text='I have a small workshop in my basement where I have built or fixed many things such as repairing a large sign for a sales booth at the Philadelphia Flower Show.  I built the beehive in this picture as well as several others and the components needed to have a sucessful apiary.'
               label='Wood Working'
-              path='#'
+              path=''
+              target=''
+              rel=''
             />
-            <AboutMeCardItem
+            <CardItem
               src='images/hobbies/homestead.jpg'
               alt='a young black australorp chicken'
               text='One of my personal goals is to become as self-sufficient as possible.  To help achieve this goal, we raised a small flock of chickens and started a small apiary.  In the winter, I tap my maple trees to collect sap and boil it down to delicious maple syrup.  I also have a large garden, many berry plants, an assortment of fruiting trees, and grape vines.'
               label='Homesteading'
-              path='#'
+              path=''
+              target=''
+              rel=''
             />
           </ul>
 
